2b71706c29889186e54f8aca0b8a45d46ffc05e0
Shan Shan Huang <91642948+shansdurgut@users.noreply.github.com>
2021-10-14 13:25:01 -0500

    Add boilerplate, footer & navbar; Edited pages to make it look nicer

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    location: String,
+    image: String,
     description: String,
-    location: String
+    price: Number
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -20,15 +20,21 @@ const sample = (array) => array[Math.floor(Math.random()*array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
+    const price = Math.floor(Math.random()*20 +10);
     for(let i =0; i < 50; i++){
         const random1000 = Math.floor(Math.random()*1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state} `,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image:'https://source.unsplash.com/collection/1315965',
+            description:"<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid animi blanditiis dolor dolore earum\n" +
+                "    facere fugiat id illum ipsa nemo pariatur placeat praesentium, provident quasi quisquam suscipit ullam vel.</div>",
+            price: price
         });
         await camp.save();
     };
 };
+
 
 seedDB().then(() => {
    mongoose.connection.close();
